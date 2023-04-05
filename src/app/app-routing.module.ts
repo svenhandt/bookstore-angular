127d49dc9c470f4f86c15142f0206761cb78dc26
Svenhandt1979 <svenhandt@gmx.de>
2023-04-05 14:14:56 +0200

    weitere Entwicklung

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from "./pages/homepage/homepage.component";
 import {ProductListPageComponent} from "./pages/product-list-page/product-list-page.component";
+import {ProductDetailsPageComponent} from "./pages/product-details-page/product-details-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomepageComponent},
   {path: 'product-list', component: ProductListPageComponent},
+  {path: 'product-details', component: ProductDetailsPageComponent},
 ];
 
 @NgModule({
