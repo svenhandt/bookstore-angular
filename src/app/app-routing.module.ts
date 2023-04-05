74cc57f35a1db9857f0eb1e1caef146f343ffc79
Svenhandt1979 <svenhandt@gmx.de>
2023-04-05 14:57:09 +0200

    Entwicklung

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from "./pages/homepage/homepage.component";
 import {ProductListPageComponent} from "./pages/product-list-page/product-list-page.component";
 import {ProductDetailsPageComponent} from "./pages/product-details-page/product-details-page.component";
+import {CartPageComponent} from "./pages/cart-page/cart-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomepageComponent},
   {path: 'product-list', component: ProductListPageComponent},
   {path: 'product-details', component: ProductDetailsPageComponent},
+  {path: 'cart', component: CartPageComponent},
 ];
 
 @NgModule({
